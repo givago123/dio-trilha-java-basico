ef5117ccba4b56abff9c53b2f78b67788006849d
givago123 <givago.mubarak1@gmail.com>
2023-10-25 14:38:05 -0300

    modificações acrescentando codigos

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,26 @@
-public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+import java.util.Scanner;
+
+public class ContaTerminal{
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        int numero;
+        String nome, agencia;
+        Float saldo;
+        try (Scanner teclado = new Scanner(System.in)) {
+            System.out.println("Por favor, digite o numero da sua conta!");
+            numero = teclado.nextInt();
+
+            System.out.println("Por favor, digite o numero da sua Agência!");
+            agencia = teclado.next();
+
+            System.out.println("Por favor, digite o nome do cliente!");
+            nome = teclado.next();
+
+            System.out.println("O saldo em sua conta");
+            saldo = teclado.nextFloat();
+            
+        }
+     }
 }
